ff1f1605039e74878c1c41ffd074ea280201028c
emanuele <emattina97@gmail.com>
2021-04-10 01:40:04 +0200

    Carico filtro per genere musicale da correggere

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ var app   = new Vue ({
     el: '#root',
     data: {
         diskActive: 0,
+        diskFiltered: 0,
         selectedGenre: '',
         index: 0,
         timeRange: 0,
         volumeRange: 20,
+        pausePlay: false,
         disks: [
             {
 
@@ -21,26 +23,49 @@ var app   = new Vue ({
         });
     },
     created() {
-        setInterval(() => {
-            this.timeRange = this.timeRange + 1;
-        }, 1000);
+        
     },
+    computed: {
+        filteredDisks() {
+
+            if(this.selectedGenre === '') {
+                return this.disks;
+            } else {
+                return this.disks
+                           .filter((disk) => disk.genre === this.selectedGenre)
+            }
+        }
+      },
     methods: {
         next() {
             this.timeRange = 0;
             if(this.diskActive < this.disks.length - 1) {
                 this.diskActive++;
-            } else {
+            } else if (this.diskActive >= this.disks.length - 1) {
                 this.diskActive = 0;
             }
         },
         prev() {
             this.timeRange = 0;
             if(this.diskActive > 0) {
-                this.diskActive--;
-            } else {
+                this.diskActive --;
+            } else if (this.diskActive <= 0){
                 this.diskActive = this.disks.length - 1;
             }
         },
+        startPlayer() {
+            setInterval(() => {
+            
+                this.timeRange = this.timeRange + 1;
+                if(this.timeRange == 100) {
+                    this.timeRange = 0;
+                }
+            }, 1000);
+
+            this.pausePlay = !this.pausePlay;
+        },
+        pausePlayer() {
+            this.pausePlay = !this.pausePlay;
+        }
     }
 });
