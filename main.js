c2adaa7545b7d8f40a6b61a65315c953be0da104
emanuele <emattina97@gmail.com>
2021-04-09 16:14:21 +0200

    Carico bozza esercizio

--- main.js
+++ main.js
@@ -2,16 +2,35 @@ Vue.config.devtools = true;
 var app   = new Vue ({
     el: '#root',
     data: {
+        index: 0,
+        timeRange: 90,
+        volumeRange: 20,
         disks: [
             {
 
             }
         ],
     },
-    mounted: {
-
+    mounted() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((response) => {
+            this.disks = (response.data.response);
+        });
     },
     methods: {
-        
+        next() {
+            if(this.index < this.disks.length - 1) {
+                this.index++;
+            } else {
+                this.index = 0;
+            }
+        },
+        prev() {
+            if(this.index > 0) {
+                this.index--;
+            } else {
+                this.index = this.disks.length - 1;
+            }
+        },
     }
 });
